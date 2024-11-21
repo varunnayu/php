b210f182810f1ac3b8b4bc9d18bb43568641daae
varunnayu <127419415+varunnayu@users.noreply.github.com>
2024-11-21 22:19:21 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $title = "My Portfolio";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
